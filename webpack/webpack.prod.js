2f3a02cdd29f0a31be5a9c800338da872e80f215
cleison alexandre azevedo pereira <cleison.alexandre.azevedo@gmail.com>
2021-04-23 00:31:50 -0300

    correction of form file

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,6 +1,5 @@
 const webpack = require('webpack');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
-.BundleAnalyzerPlugin
+
 module.exports = {
     mode: 'production',
     devtool: 'source-map',
@@ -10,6 +9,6 @@ module.exports = {
                 'process.env.name': JSON.stringify('Utilizando webpack prod'),
             }
         ),
-        new BundleAnalyzerPlugin(),
+        
     ]
 }
