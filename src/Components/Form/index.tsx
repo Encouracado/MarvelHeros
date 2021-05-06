9e1362e81510e8c5c2f5ea455583cf01ae236ded
cleison alexandre azevedo pereira <cleison.alexandre.azevedo@gmail.com>
2021-05-06 01:20:33 -0300

    remove try catch

--- src/Components/Form/index.tsx
+++ src/Components/Form/index.tsx
@@ -30,23 +30,18 @@ const Form: React.FC = () => {
   const onSubmit = async function (data: Inputs) {
     let hashKeys = md5(ts + data.privateKey + data.publicKey)
 
-    try {
-      const dataMarvel: Hero = await api.get('characters', {
-        params: {
-          ts,
-          apikey: data.publicKey,
-          hash: hashKeys,
-        },
-      })
-      // eslint-disable-next-line no-use-before-define
-      dataMarvel.data.data.results.map((hero: Hero) => {
-        heroList.push(hero)
-        history.push('/listHero')
-      })
-    } catch (e) {
-      history.push('/')
-      alert('Alguma das chaves está incorreta')
-    }
+    const dataMarvel: Hero = await api.get('characters', {
+      params: {
+        ts,
+        apikey: data.publicKey,
+        hash: hashKeys,
+      },
+    })
+    // eslint-disable-next-line no-use-before-define
+    dataMarvel.data.data.results.map((hero: Hero) => {
+      heroList.push(hero)
+      history.push('/listHero')
+    })
   }
 
   return (
