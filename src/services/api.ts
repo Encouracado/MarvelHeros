ee2d0dba7b88292317d2895608dce21fb696c747
cleison alexandre azevedo pereira <cleison.alexandre.azevedo@gmail.com>
2021-05-12 10:54:52 -0300

    removed keys from axios instance

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,13 +1,6 @@
 import axios from 'axios';
 
 
-const privateKey = '4d89f732f2070f767cd167c90974c9722f2c6e19';
-
-const publicKey = '4da92f9ba2c7b05eaf1e088dbffe0ce7';
-
-let privateK = ""
-let publicK = ""
-
 const api = axios.create({
   baseURL: 'http://gateway.marvel.com/v1/public/',
 });
